bf3591533acff91ae064b3973dcb9183a0452472
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2026-06-25 23:16:57 -0600

    update code for conditional logic, indentation in python

---
+++ python-basics-2/conditional_logic.py
@@ -0,0 +1,8 @@
+is_old = True
+is_licenced = False
+
+if is_old and is_licenced:
+    print("you are old enough to drive, and you have a license!")
+else:
+    print("you are not of age!")
+    print("okoko")
