cf123556f27d2e7d704151835ce205b45e7a861d
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2026-07-07 21:16:32 -0600

    update code regular expressions of section regular expressions

---
+++ regular-expressions/regular_expressions.py
@@ -0,0 +1,21 @@
+import re
+
+pattern = re.compile("search this inside of this text please")
+string = "search this inside of this text please! Vinh Vong"
+
+# match object when using regular expression
+a = pattern.search(string)
+b = pattern.findall(string)
+c = pattern.fullmatch(string)
+d = pattern.match(string)
+
+# print(a.span())  # return a tuple showing the index of found (17, 21)
+# print(a.start())  # when the text found start
+# print(a.end())  # when the text found end
+# print(a.group())
+# print(b)
+
+# return object only full match, the search string and compile is exactly the same
+# print(c)
+
+print(d)
